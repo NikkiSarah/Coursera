c50c1d27a3eb9cfde3659b6e4625b6cfd16c1c2b
NikkiSarah <81358254+NikkiSarah@users.noreply.github.com>
2023-12-11 14:16:32 +1100

    Add files via upload

--- _Machine Learning_Theory and Hands-On Practice with Python (University of Colorado Boulder)/2. Unsupervised Algorithms in Machine Learning/movie_ratings_NNMF.py
+++ _Machine Learning_Theory and Hands-On Practice with Python (University of Colorado Boulder)/2. Unsupervised Algorithms in Machine Learning/movie_ratings_NNMF.py
@@ -8,15 +8,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from collections import namedtuple
 from scipy.sparse import csr_matrix, csr_array
-from sklearn.decomposition import MiniBatchNMF, NMF
+from sklearn.decomposition import MiniBatchNMF
 from sklearn.metrics import mean_squared_error
 
 # load the data
-MV_users = pd.read_csv('data/movie-lens-1m/users.csv')
-MV_movies = pd.read_csv('data/movie-lens-1m/movies.csv')
-train = pd.read_csv('data/movie-lens-1m/train.csv')
-test = pd.read_csv('data/movie-lens-1m/test.csv')
+MV_users = pd.read_csv('data/movielens-1m/users.csv')
+MV_movies = pd.read_csv('data/movielens-1m/movies.csv')
+train = pd.read_csv('data/movielens-1m/train.csv')
+test = pd.read_csv('data/movielens-1m/test.csv')
 
 MV_users.rename(columns={'accupation': 'occupation'}, inplace=True)
 
@@ -74,6 +75,8 @@ average_ratings = MV_movies_ratings.groupby('genre')['rating'].mean().reset_inde
 average_ratings.sort_values(by='rating', inplace=True)
 
 sns.barplot(average_ratings, x='rating', y='genre', color='seagreen')
+plt.title("Average (mean) movie ratings by genre")
+sns.despine()
 
 # the most common occupations in the dataset are Executive/Managerial, Other and College/Grad student. The least
 # common occupation is Farmer
@@ -109,76 +112,133 @@ sns.countplot(MV_users, y="age", color="seagreen")
 plt.title("Distribution of user counts by age")
 sns.despine()
 
-# set up objects
-allusers = list(MV_users.uID)
-allmovies = list(MV_movies.mID)
-mid2idx = dict(zip(MV_movies.mID, list(range(len(MV_movies)))))
-uid2idx = dict(zip(MV_users.uID, list(range(len(MV_users)))))
-FILL_VALUE = 3.5
+
+# define the NMF RecSys class
+class NMFRecSys():
+    def __init__(self, data):
+        self.data = data
+        self.allusers = list(self.data.users.uID)
+        self.allmovies = list(self.data.movies.mID)
+        self.mid2idx = dict(zip(self.data.movies.mID, list(range(len(self.data.movies)))))
+        self.uid2idx = dict(zip(self.data.users.uID, list(range(len(self.data.users)))))
+        self.Mr = self.rating_matrix()
+        self.yt = self.test_rating_matrix()
+
+
+    def rating_matrix(self, fill_value=0):
+        """
+        Generates a numpy ratings array using the training data.
+        :return: Numpy array with shape (#allusers, #allmovies)
+        """
+        # explode into sparse matrices with user ids as the rows and movie ids as the columns
+        ind_movie = [self.mid2idx[id] for id in self.data.train.mID]
+        ind_user = [self.uid2idx[id] for id in self.data.train.uID]
+        rating_train = list(self.data.train.rating)
+        Mr = np.full(shape=(len(self.allusers), len(self.allmovies)), fill_value=fill_value)
+        Mr[ind_user, ind_movie] = rating_train
+
+        # assert that the generated ratings matrix and the check matrix are the same
+        # note that it fails when the fill_value != 0
+        if fill_value == 0:
+            Mr_check = csr_matrix(self.data.train.pivot(index='uID', columns='mID', values='rating').fillna(fill_value))
+            assert Mr.sum() == Mr_check.sum(), "Check ratings matrix!"
+        else:
+            pass
+
+        return Mr
+
+
+    def test_rating_matrix(self, fill_value=0):
+        """
+        Generates a numpy ratings array using the test data.
+        :return: Numpy array with shape (#allusers, #allmovies)
+        """
+        # explode into sparse matrices with user ids as the rows and movie ids as the columns
+        ind_movie = [self.mid2idx[id] for id in self.data.test.mID]
+        ind_user = [self.uid2idx[id] for id in self.data.test.uID]
+        rating_test = list(self.data.test.rating)
+        yt = np.full(shape=(len(self.allusers), len(self.allmovies)), fill_value=fill_value)
+        yt[ind_user, ind_movie] = rating_test
+
+        # assert that the generated ratings matrix and the check matrix are the same
+        if fill_value == 0:
+            yt_check = csr_matrix(self.data.test.pivot(index='uID', columns='mID', values='rating').fillna(fill_value))
+            assert yt.sum() == yt_check.sum(), "Check ratings matrix!"
+        else:
+            pass
+
+        return yt
+
+
+    def predict(self, num_features=20, init_method='nndsvd', max_iterations=500, batch_size=1024):
+        """
+        NMF was developed from PCA and is designed to extract sparse and significant features from a set of
+        non-negative data factors. It decomposes a non-negative matrix (Mr) into its user (W) and movie (H) component
+        matrices by optimising the distance between Mr and the matrix product of the component matrices. The most
+        widely used distance measure (and the default in sklearn) is the squared Frobenius norm.
+        The number of latent or hidden features must be pre-specified, and is the second dimension of the component
+        matrices.
+
+        :param num_features: The desired number of latent or hidden features for the component matrices W and H.
+        :param init_method: Initialisation method. 'nnsvd' is better for sparseness, but 'nnsvda' or 'random' by default.
+        :param max_iterations: Maximum number of iterations permitted.
+        :param batch_size: Number of samples in each mini-batch. Larger batches provide better long-term convergence.
+        :return: A sparse rating prediction array with shape (#allusers, #allmovies)
+        """
+        # model = NMF(n_components=num_features, init=init_method, random_state=42, max_iter=max_iterations)
+        model = MiniBatchNMF(n_components=num_features, init=init_method, random_state=42, max_iter=max_iterations,
+                             batch_size=batch_size)
+
+        W = model.fit_transform(self.Mr)  # shape (#users, #latent features)
+        H = model.components_  # shape (#latent features, #movies)
+
+        # construct the prediction matrix
+        yp = np.dot(W, H)
+        # apply a threshold to coerce small values to be 0
+        # yp[yp < 1] = 0
+        yp = csr_array(yp)
+
+        return yp
+
+
+    def rmse(self, yp, fill_value=0):
+        """
+        Generate the RMSE of the test ratings data.
+        :param yt: Actual ratings
+        :param yp: Predicted ratings
+        :return: RMSE score
+        """
+
+        # identify non-missing actual ratings in the dataset
+        # non_missing_idx = yt.nonzero()
+        non_missing_idx = (self.yt != fill_value).nonzero()
+        rmse = np.sqrt(mean_squared_error(self.yt[non_missing_idx], yp[non_missing_idx]))
+
+        return rmse
+
+
+Data = namedtuple('Data', ['users', 'movies', 'train', 'test'])
+data = Data(MV_users, MV_movies, train, test)
 
 # sample the data
-recsys_train = train[:30000]
-recsys_test = test[:30000]
-# use all the data
-recsys_train = train.copy()
-recsys_test = test.copy()
+sample_train = train[:30000]
+sample_test = test[:30000]
 
-def set_up_recsys(recsys_train, recsys_test):
-    # identify all the users and movies in the sampled training and testing data combined
-    recsys_MV_users = MV_users[(MV_users.uID.isin(recsys_train.uID)) | (MV_users.uID.isin(recsys_test.uID))]
-    recsys_MV_movies = MV_movies[(MV_movies.mID.isin(recsys_train.mID)) | (MV_movies.mID.isin(recsys_test.mID))]
+# identify all the users and movies in the sampled training and testing data combined
+sample_MV_users = MV_users[(MV_users.uID.isin(sample_train.uID)) | (MV_users.uID.isin(sample_test.uID))]
+sample_MV_movies = MV_movies[(MV_movies.mID.isin(sample_train.mID)) | (MV_movies.mID.isin(sample_test.mID))]
 
-    # explode into sparse matrices with user ids as the rows and movie ids as the columns
-    ind_movie = [mid2idx[id] for id in recsys_train.mID]
-    ind_user = [uid2idx[id] for id in recsys_train.uID]
-    rating_train = list(recsys_train.rating)
-    Mr = np.full(shape=(len(allusers), len(allmovies)), fill_value=FILL_VALUE)
-    Mr[ind_user, ind_movie] = rating_train
+sample_data = Data(sample_MV_users, sample_MV_movies, sample_train, sample_test)
 
-ind_movie = [mid2idx[id] for id in recsys_test.mID]
-ind_user = [uid2idx[id] for id in recsys_test.uID]
-rating_test = list(recsys_test.rating)
-yt = np.full(shape=(len(allusers), len(allmovies)), fill_value=FILL_VALUE)
-yt[ind_user, ind_movie] = rating_test
-yt = csr_array(yt)
+# test on the sample data
+sample_rs = NMFRecSys(sample_data)
+sample_yp = sample_rs.predict(num_features=20)
+print('RMSE:', round(sample_rs.rmse(sample_yp), 4))
 
-#double check the matrices are correct (if the FILL_VALUE != 0 then the assertions fail)
-Mr_check = csr_matrix(recsys_train.pivot(index='uID', columns='mID', values='rating').fillna(FILL_VALUE))
-yt_check = csr_matrix(recsys_test.pivot(index='uID', columns='mID', values='rating').fillna(FILL_VALUE))
-
-assert Mr.sum() == Mr_check.sum()
-assert yt.sum() == yt_check.sum()
-
-# NMF was developed from PCA and is designed to extract sparse and significant features from a set of non-negative
-# data factors. It decomposes a non-negative matrix (Mr) into its user (W) and movie (H) component matrices by
-# optimising the distance between Mr and the matrix product of the component matrices. The most widely used distance
-# measure (and the default in sklearn) is the squared Frobenius norm.
-# The number of latent or hidden features must be pre-specified, and is the second dimension of the component matrices.
-# It can be at most equal to the total number of movies in Mr.
-init_method = [None, 'random', 'nndsvd', 'nnsvda']
-BATCH_SIZE = 1024 # larger batches give better long-term convergence at the cost of a slower start
-MAX_ITER = 500
-
-for val in [5, 10, 20, 50]:
-    # model = NMF(n_components=val, init=init_method[2], random_state=42, max_iter=MAX_ITER)
-    model = MiniBatchNMF(n_components=val, init=init_method[2], random_state=42, max_iter=MAX_ITER,
-                         batch_size=BATCH_SIZE)
-
-    W = model.fit_transform(Mr)     # shape (#users, #latent features)
-    H = model.components_           # shape (#latent features, #movies)
-
-    # construct the prediction matrix
-    yp = np.dot(W, H)
-    # apply a threshold to coerce small values to be 0
-    # yp[yp < 1] = 0
-    yp = csr_array(yp)
-
-    # identify non-missing actual ratings in the dataset
-    # non_missing_idx = yt.nonzero()
-    non_missing_idx = (yt != FILL_VALUE).nonzero()
-    rmse = np.sqrt(mean_squared_error(yt[non_missing_idx], yp[non_missing_idx]))
-
-    print('Latent features:', val, '; RMSE:', round(rmse, 6))
+# run on the full data
+rs = NMFRecSys(data)
+yp = rs.predict(num_features=20)
+print('RMSE:', round(rs.rmse(yp), 4))
 
 # Recall that the results from the previous recommender systems were:
 # |Method|RMSE sample|RMSE|
@@ -193,16 +253,15 @@ for val in [5, 10, 20, 50]:
 
 # |Method|RMSE sample|RMSE|
 # |:----|:--------:|:--:|
-# |Collaborative, 0 fill|3.73| |
-# |Collaborative, 3.5 fill|1.12| |
+# |Collaborative, 0 fill|3.7307|2.9127|
+# |Collaborative, 3.5 fill|1.1220|1.0473|
 
-# The RMSE on the sample data was 3.7 and the RMSE on the full dataset was x. NMF performs poorly because 'missing'
+# The RMSE on the sample data was 3.7 and the RMSE on the full dataset was 2.9. NMF performs poorly because 'missing'
 # ratings are automatically set to 0, which is outside the range of valid values (1-5) and adversely affects the loss
-# Frobenius norm function. Changing the fill value to a non-zero value appeared to help immensely, but a single value
-# had to be used, which isn't ideal as it's a relatively crude approach. The best solution would be to simply avoid
-# including missing rating data in the loss function calculation, but that's not possible using the sklearn
-# implementation of NMF.
-
-# What could also help would be increasing the number of components (although at lower values, RMSE appeared to be
-# relatively insensitive to changes). This wasn't experimented with here due to computational resource (power and time)
-# constraints.
+# Frobenius norm function. Changing the fill value to a non-zero value appeared to help immensely, but only insofar as
+# to become comparable with the baseline results.
+#
+# The best solution to the problem would be to simply avoid including missing ratings data in the loss function
+# calculation, but this isn't possible using the current implementation of sklearn's NMF (or MiniBatchNMF) function.
+# Other things that could help at the marhins would be experimenting with increasing the number of components or
+# latent features retained and developing a more refined approach to imputing the missing ratings data.
